6c2ae29e58bd92b26535b7e5959329b53e404f8b
chimo <chimo@chromic.org>
2023-09-28 21:12:31 -0400

    Add ugly edgeos part
    TODO: refactor to OOP, modeled after the Unifi_API\Client

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ function get_wifi_info($config) {
     $unifi_connection = new UniFi_API\Client(
         $config['username'],
         $config['password'],
-        $config['url'], 
+        $config['url'],
         $config['site_id'],
         $config['version'],
         true
@@ -18,6 +18,7 @@ function get_wifi_info($config) {
     $unifi_connection->login();
 
     $clients = $unifi_connection->list_clients();
+
     $essids = array();
 
     foreach ($clients as $client) {
@@ -35,8 +36,71 @@ function get_wifi_info($config) {
 };
 
 
+function edgeos_login($api_root, $username, $password) {
+    $ch = curl_init();
+    $headers = [];
+
+    curl_setopt($ch, CURLOPT_URL,$api_root);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        http_build_query(array(
+            'username' => $username,
+            'password' => $password
+        )));
+
+    curl_setopt($ch, CURLOPT_HEADERFUNCTION,
+        function($curl, $header) use (&$headers)
+        {
+            $len = strlen($header);
+            $header = explode(':', $header, 2);
+            if (count($header) < 2)
+                return $len;
+
+            $headers[strtolower(trim($header[0]))][] = trim($header[1]);
+
+            return $len;
+        }
+    );
+
+    curl_exec($ch);
+    curl_close($ch);
+
+    $cookies = $headers['set-cookie'];
+    $cookie_str = '';
+
+    foreach ($cookies as $cookie) {
+        $cookie_str .= $cookie . ' ';
+    }
+
+    $cookie_str = trim($cookie_str);
+
+    return $cookie_str;
+};
+
+
+function edgeos_get_data($api_root, $data_type, $cookies) {
+    $url = $api_root . '/api/edge/data.json?data=' . $data_type;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    $data = curl_exec($ch);
+
+    return $data;
+};
+
+
 function get_lan_info($config) {
-    return 'placeholder';
+    $api_root = $config['url'];
+    $cookies = edgeos_login($api_root,
+                            $config['username'],
+                            $config['password']);
+
+    $dhcp_leases = edgeos_get_data($api_root, 'dhcp_leases', $cookies);
+
+    return $dhcp_leases;
 };
 
 
