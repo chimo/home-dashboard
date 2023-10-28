5158945083759d599b66fd4f785bc53bf393f6d8
chimo <chimo@chromic.org>
2023-10-28 09:24:17 -0400

    Add some getters

--- private/edgeos_lib.php
+++ private/edgeos_lib.php
@@ -41,10 +41,123 @@ class Client
     }
 
 
+    public function get_dhcp_stats() {
+        $data = $this->get_data('dhcp_stats');
+
+        return $data;
+    }
+
+
+    public function get_firewall_stats() {
+        $data = $this->get_data('fw_stats');
+
+        return $data;
+    }
+
+
+    public function get_gpon_onu_list() {
+        $data = $this->get_data('gpon_onu_list');
+
+        return $data;
+    }
+
+
+    public function get_nat_stats() {
+        $data = $this->get_data('nat_stats');
+
+        return $data;
+    }
+
+
+    public function get_routes() {
+        $data = $this->get_data('routes');
+
+        return $data;
+    }
+
+
+    public function get_sys_info() {
+        $data = $this->get_data('sys_info');
+
+        return $data;
+    }
+
+
+    public function is_default_config() {
+        $data = $this->get_data('default_config');
+
+        return ($data === '1');
+    }
+
+
     public function get_dhcp_leases() {
         $data = $this->get_data('dhcp_leases');
 
         return $data;
+    }
+
+
+    // NOTE: Need to be admin, otherwise HTTP 403 is returned
+    public function get_configurations($node = null) {
+        $query = '';
+
+        if ($node !== null) {
+            $query = urlencode('?' . $node);
+        }
+
+        $url = $this->baseurl . '/api/edge/getcfg.json' . $query;
+
+        echo $url;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIE, $this->cookies);
+
+        $data = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        error_log($http_code);
+
+        return json_decode($data);
+    }
+
+
+    // FIXME: /api/edge/partial.json returns a 404
+    //        Investigate.
+    public function get_settings_section($struct) {
+        $query = urlencode('?struct=' . $struct);
+        $url = $this->baseurl . '/api/edge/partial.json' . $query;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIE, $this->cookies);
+
+        $data = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        error_log($http_code);
+
+        return json_decode($data);
+    }
+
+
+    // Predefined, hardcoded list of settings
+    public function get_settings() {
+        $url = $this->baseurl . '/api/edge/get.json';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIE, $this->cookies);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($data);
     }
 
 
